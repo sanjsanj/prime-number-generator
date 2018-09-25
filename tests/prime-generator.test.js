12aa36bd11a9f6f8c5b71fc76b9fa3d9f97a4ef0
Sanjay Purswani <sanjsanj@hotmail.com>
2018-09-25 19:21:49 +0100

    primeGenerator can return 1,000 primes, refactor out large arrays to helpers

--- tests/prime-generator.test.js
+++ tests/prime-generator.test.js
@@ -1,16 +1,20 @@
 const { primeGenerator } = require("../src/prime-generator");
+const { tenPrimes, thousandPrimes } = require("./helpers");
 
 describe("primeGenerator", () => {
   it("should return array of 1 prime number", () => {
     expect(primeGenerator(1)).toEqual([2]);
   });
-  
+
   it("should return array of 2 prime numbers", () => {
     expect(primeGenerator(2)).toEqual([2, 3]);
   });
-  
+
   it("should return array of 10 prime numbers", () => {
-    const tenPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
     expect(primeGenerator(10)).toEqual(tenPrimes);
+  });
+
+  it("should return array of 1,000 prime numbers", () => {
+    expect(primeGenerator(1000)).toEqual(thousandPrimes);
   });
 });
