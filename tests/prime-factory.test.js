77b563f7e834f44f21eb5cdd2c41c0ad036c3d96
Sanjay Purswani <sanjsanj@hotmail.com>
2018-09-25 20:13:34 +0100

    REFACTOR: Nest generate tests in their own describe block to make adding tests for next function easier

--- tests/prime-factory.test.js
+++ tests/prime-factory.test.js
@@ -1,30 +1,32 @@
 const { PrimeFactory } = require("../src/prime-factory");
 const { tenPrimes, thousandPrimes } = require("./helpers");
 
-describe("PrimeFactory().generate", () => {
-  let generate;
+describe("PrimeFactory", () => {
+  describe("generate", () => {
+    let generate;
 
-  beforeEach(() => {
-    generate = PrimeFactory().generate;
-  });
+    beforeEach(() => {
+      generate = PrimeFactory().generate;
+    });
 
-  it("should return array of 1 prime number", () => {
-    expect(generate(1)).toEqual([2]);
-  });
+    it("should return array of 1 prime number", () => {
+      expect(generate(1)).toEqual([2]);
+    });
 
-  it("should return array of 2 prime numbers", () => {
-    expect(generate(2)).toEqual([2, 3]);
-  });
+    it("should return array of 2 prime numbers", () => {
+      expect(generate(2)).toEqual([2, 3]);
+    });
 
-  it("should return array of 10 prime numbers", () => {
-    expect(generate(10)).toEqual(tenPrimes);
-  });
+    it("should return array of 10 prime numbers", () => {
+      expect(generate(10)).toEqual(tenPrimes);
+    });
 
-  it("should return array of 1,000 prime numbers", () => {
-    expect(generate(1000)).toEqual(thousandPrimes);
-  });
+    it("should return array of 1,000 prime numbers", () => {
+      expect(generate(1000)).toEqual(thousandPrimes);
+    });
 
-  it("should return array of 20,000 prime numbers", () => {
-    expect(generate(20000).length).toEqual(20000);
+    it("should return array of 20,000 prime numbers", () => {
+      expect(generate(20000).length).toEqual(20000);
+    });
   });
 });
