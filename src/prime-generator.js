f2bf5c10f878e64afeb8a5fc05bf3cb844cdabde
Sanjay Purswani <sanjsanj@hotmail.com>
2018-09-25 19:13:17 +0100

    primeGenerator can return 10 primes, using non-prod brute force code

--- src/prime-generator.js
+++ src/prime-generator.js
@@ -1,5 +1,24 @@
-module.exports = {
-  primeGenerator(num) {
-    return num === 1 ? [2] : [2, 3];
+function primeGenerator(num) {
+  const primesArray = [];
+  let currentNum = 2;
+
+  while (primesArray.length < num) {
+    if (_isPrime(currentNum)) {
+      primesArray.push(currentNum);
+    }
+
+    currentNum++;
   }
-};
+
+  return primesArray;
+}
+
+function _isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
+
+module.exports = { primeGenerator };
--- tests/prime-generator.test.js
+++ tests/prime-generator.test.js
@@ -8,4 +8,9 @@ describe("primeGenerator", () => {
   it("should return array of 2 prime numbers", () => {
     expect(primeGenerator(2)).toEqual([2, 3]);
   });
+  
+  it("should return array of 10 prime numbers", () => {
+    const tenPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    expect(primeGenerator(10)).toEqual(tenPrimes);
+  });
 });
